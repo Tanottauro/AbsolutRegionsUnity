5cebeb0321e9924dbcd09d299ffce63e66997dc9
Tanottauro <Tanottauro@github.com>
2021-03-02 02:15:56 -0300

    Primeiro update
    Fazendo update do projeto, o que eu fiz até agora.

---
+++ Assets/Script/FirebaseManager.cs
@@ -0,0 +1,420 @@
+﻿using System.Collections;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
+using System;
+using System.Linq;
+using TMPro;
+
+public class FirebaseManager : MonoBehaviour
+{    
+    [Header("Variaveis do Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser User;
+    public DatabaseReference DBreference;
+
+    
+    [Header("Variaveis de Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+
+    [Header("Variaveis de Registro")]
+    public TMP_InputField usernameRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
+    public TMP_InputField passwordRegisterVerifyField;
+    public TMP_Text warningRegisterText;
+
+    [Header("Variaveis De Usuários")]
+    public TMP_Text usernameText;
+    public TMP_Text xpText;
+    public TMP_Text lvlText;
+    public TMP_Text moneyText;
+    public TMP_Text goldText;
+    public GameObject scoreElement;
+    public Transform scoreboardContent;
+
+    [Header("Recursos Do Usuário")]
+    private int moneyPlayer;
+    private int goldPlayer;
+
+    [Header("Recursos Produzidos Nas Minas")]
+    public int clay;
+    public int limestone;
+    public int oil;
+    public int iron;
+    public int coal;
+    public int copper;
+    public int cobalt;
+    public int uranium;
+    public int Titanium;
+    public int Nickel;
+    public int Aluminum;
+
+    [Header("Recursos Processados em Fabricas")]
+    public int brick;
+    public int steel;
+    public int concrete;
+    public int steelBeam;
+    public int ironRod;
+    public int copperBar;
+    public int titaniumAlloy;
+    public int combustible;
+
+    [Header("Recursos Processados em Fabricas Militares")]
+    public int dogs;
+    public int shoppingSecurity;
+
+    //[Header("Implementar Recuros")]
+    //private double getMoney = 0;
+
+    void Awake()
+    {
+        //Check that all of the necessary dependencies for Firebase are present on the system
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                //If they are avalible Initialize Firebase
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
+    private void InitializeFirebase()
+    {
+        Debug.Log("Setting up Firebase Auth");
+        //Set the authentication instance object
+        auth = FirebaseAuth.DefaultInstance;
+        FirebaseApp.DefaultInstance.Options.DatabaseUrl = new Uri("https://regions-absolut-default-rtdb.firebaseio.com/");
+        DBreference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public void ClearLoginFeilds()
+    {
+        emailLoginField.text = "";
+        passwordLoginField.text = "";
+    }
+    public void ClearRegisterFeilds()
+    {
+        usernameRegisterField.text = "";
+        emailRegisterField.text = "";
+        passwordRegisterField.text = "";
+        passwordRegisterVerifyField.text = "";
+    }
+
+    //Function for the login button
+    public void LoginButton()
+    {
+        //Call the login coroutine passing the email and password
+        StartCoroutine(Login(emailLoginField.text, passwordLoginField.text));
+    }
+    //Function for the register button
+    public void RegisterButton()
+    {
+        //Call the register coroutine passing the email, password, and username
+        StartCoroutine(Register(emailRegisterField.text, passwordRegisterField.text, usernameRegisterField.text));
+    }
+
+    public void SignOutButton()
+    {
+        auth.SignOut();
+        UIManager.instance.LoginScreen();
+        ClearRegisterFeilds();
+        ClearLoginFeilds();
+    }
+
+    public void SaveDataButton()
+    {
+        StartCoroutine(UpdateUsernameAuth(usernameText.text));
+        StartCoroutine(UpdateUsernameDatabase(usernameText.text));
+
+        StartCoroutine(UpdateXp(int.Parse(xpText.text)));
+        StartCoroutine(UpdateLvl(int.Parse(lvlText.text)));
+        StartCoroutine(UpdateMoney(int.Parse(moneyText.text)));
+    }
+
+    private IEnumerator Login(string _email, string _password)
+    {
+        //Call the Firebase auth signin function passing the email and password
+        var LoginTask = auth.SignInWithEmailAndPasswordAsync(_email, _password);
+        //Wait until the task completes
+        yield return new WaitUntil(predicate: () => LoginTask.IsCompleted);
+
+        if (LoginTask.Exception != null)
+        {
+            //If there are errors handle them
+            Debug.LogWarning(message: $"Failed to register task with {LoginTask.Exception}");
+            FirebaseException firebaseEx = LoginTask.Exception.GetBaseException() as FirebaseException;
+            AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+            string message = "Login Failed!";
+            switch (errorCode)
+            {
+                case AuthError.MissingEmail:
+                    message = "Missing Email";
+                    break;
+                case AuthError.MissingPassword:
+                    message = "Missing Password";
+                    break;
+                case AuthError.WrongPassword:
+                    message = "Wrong Password";
+                    break;
+                case AuthError.InvalidEmail:
+                    message = "Invalid Email";
+                    break;
+                case AuthError.UserNotFound:
+                    message = "Account does not exist";
+                    break;
+            }
+            warningLoginText.text = message;
+        }
+        else
+        {
+            //User is now logged in
+            //Now get the result
+            User = LoginTask.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})", User.DisplayName, User.Email);
+            warningLoginText.text = "";
+            confirmLoginText.text = "Logado com sucesso, carregando";
+            StartCoroutine(LoadUserData());
+
+            yield return new WaitForSeconds(2);
+
+            usernameText.text = User.DisplayName;
+            UIManager.instance.UserDataScreen(); // Change to user data UI
+            confirmLoginText.text = "";
+            ClearLoginFeilds();
+            ClearRegisterFeilds();
+        }
+    }
+
+    
+
+    private IEnumerator Register(string _email, string _password, string _username)
+    {
+        if (_username == "")
+        {
+            //If the username field is blank show a warning
+            warningRegisterText.text = "Missing Username";
+        }
+        else if (passwordRegisterField.text != passwordRegisterVerifyField.text)
+        {
+            //If the password does not match show a warning
+            warningRegisterText.text = "Password Does Not Match!";
+        }
+        else
+        {
+            //Call the Firebase auth sign in function passing the email and password
+            var RegisterTask = auth.CreateUserWithEmailAndPasswordAsync(_email, _password);
+            //Wait until the task completes
+            yield return new WaitUntil(predicate: () => RegisterTask.IsCompleted);
+
+            if (RegisterTask.Exception != null)
+            {
+                //If there are errors handle them
+                Debug.LogWarning(message: $"Failed to register task with {RegisterTask.Exception}");
+                FirebaseException firebaseEx = RegisterTask.Exception.GetBaseException() as FirebaseException;
+                AuthError errorCode = (AuthError)firebaseEx.ErrorCode;
+
+                string message = "Register Failed!";
+                switch (errorCode)
+                {
+                    case AuthError.MissingEmail:
+                        message = "Missing Email";
+                        break;
+                    case AuthError.MissingPassword:
+                        message = "Missing Password";
+                        break;
+                    case AuthError.WeakPassword:
+                        message = "Weak Password";
+                        break;
+                    case AuthError.EmailAlreadyInUse:
+                        message = "Email Already In Use";
+                        break;
+                }
+                warningRegisterText.text = message;
+            }
+            else
+            {
+                //User has now been created
+                //Now get the result
+                User = RegisterTask.Result;
+
+                if (User != null)
+                {
+                    //Create a user profile and set the username
+                    UserProfile profile = new UserProfile { DisplayName = _username };
+
+                    //Call the Firebase auth update user profile function passing the profile with the username
+                    var ProfileTask = User.UpdateUserProfileAsync(profile);
+                    //Wait until the task completes
+                    yield return new WaitUntil(predicate: () => ProfileTask.IsCompleted);
+
+                    if (ProfileTask.Exception != null)
+                    {
+                        //If there are errors handle them
+                        Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
+                        warningRegisterText.text = "Username Set Failed!";
+                    }
+                    else
+                    {
+                        //Username is now set
+                        //Now return to login screen
+                        StartCoroutine(UpdateMoney(10000));
+                        StartCoroutine(UpdateXp(10));
+                        StartCoroutine(UpdateLvl(1));
+                        UIManager.instance.LoginScreen();
+                        warningRegisterText.text = "";
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator UpdateUsernameAuth(string _username)
+    {
+        //Create a user profile and set the username
+        UserProfile profile = new UserProfile { DisplayName = _username };
+
+        //Call the Firebase auth update user profile function passing the profile with the username
+        var ProfileTask = User.UpdateUserProfileAsync(profile);
+        //Wait until the task completes
+        yield return new WaitUntil(predicate: () => ProfileTask.IsCompleted);
+
+        if (ProfileTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {ProfileTask.Exception}");
+        }
+        else
+        {
+            //Auth username is now updated
+        }
+    }
+
+    private IEnumerator UpdateUsernameDatabase(string _username)
+    {
+        //Set the currently logged in user username in the database
+        var DBTask = DBreference.Child("Users").Child(User.UserId).Child("UserInfo").Child("NickName").SetValueAsync(_username);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            //Database username is now updated
+        }
+    }
+
+    private IEnumerator UpdateXp(int _up)
+    {
+        //Set the currently logged in user xp
+        var DBTask = DBreference.Child("Users").Child(User.UserId).Child("UserInfo").Child("Xp").SetValueAsync(_up);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            //Sucesso
+        }
+    }
+    private IEnumerator UpdateMoney(int _up)
+    {
+        var DBTask = DBreference.Child("Users").Child(User.UserId).Child("UserInfo").Child("money").SetValueAsync(_up);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            //Sucesso
+        }
+    }
+
+    private IEnumerator UpdateLvl(int _up)
+    {
+
+        var DBTask = DBreference.Child("Users").Child(User.UserId).Child("UserInfo").Child("lvl").SetValueAsync(_up);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            //Sucesso
+        }
+    }
+
+    private IEnumerator UpdateIronM(int _up)
+    {
+
+        var DBTask = DBreference.Child("Users").Child(User.UserId).Child("UserInfo").Child("lvl").SetValueAsync(_up);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            //Sucesso
+        }
+    }
+
+    private IEnumerator LoadUserData()
+    {
+        //Get the currently logged in user data
+        var DBTask = DBreference.Child("Users").Child(User.UserId).GetValueAsync();
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        DataSnapshot snapshot = DBTask.Result;
+        xpText.text = snapshot.Child("xp").Value.ToString();
+        lvlText.text = snapshot.Child("lvl").Value.ToString();
+        moneyText.text = snapshot.Child("money").Value.ToString();
+
+        if (DBTask.Exception != null)
+        {
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else if (DBTask.Result.Value == null)
+        {
+            //No data exists yet
+            //xpText.text = "0";
+            //lvlText.text = "0";
+            //moneyText.text = "0";
+        }
+        else
+        {
+            xpText.text = snapshot.Child("xp").Value.ToString();
+            lvlText.text = snapshot.Child("lvl").Value.ToString();
+            moneyText.text = snapshot.Child("money").Value.ToString();
+        }
+    }
+
+    
+
+}
